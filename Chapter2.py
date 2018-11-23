d65f20d38528bced11c666c7c7ea276e50b130e2
yangchengkai <yangchengkai@yun-jinrong.com>
2018-11-23 16:46:48 +0800

    邻接列表 邻接列表表示法 与 加权邻接字典

--- Chapter2.py
+++ Chapter2.py
@@ -41,5 +41,31 @@ def graphy():
     print(b in N[a])
     print(d in N[c])
     print(N[f])
-
+    print("------列表------")
+    a, b, c, d, e, f, g, h = range(8)
+    N2 = [
+        [b, c, f, d, e],     # a
+        [c, e],              # b
+        [d],                 # c
+        [e],                 # d
+        [f],                 # e
+        [c, g, h],              # f
+        [f, h],              # g
+        [f, g],              # h
+    ]
+    print( b in N2[a])
+    a, b, c, d, e, f, g, h = range(8)
+    N3 = [
+        {b: 2, c: 1, d: 3, e: 9, f: 4},  # a
+        {c: 4, e: 3},                     # b
+        {d: 8},                              # c
+        {e: 7},                             # d
+        {f: 5},                             # e
+        {c: 2, g: 2, h: 2},                 # f
+        {f: 1, h: 6},                       # g
+        {f: 9, g: 8}                        # h
+    ]
+    print(b in N3[a])
+    print(len(N3[f]))
+    print(N3[a][b])
 graphy()
