4ac1cba6cbce3d328bf75dfb05342ff6ee874c3a
forrestlmj <18810276930@163.com>
2019-01-26 22:26:02 +0800

    743

---
+++ leetcode/743.py
@@ -0,0 +1,17 @@
+#https://leetcode.com/problems/network-delay-time/
+
+class Solution:
+    def networkDelayTime(self, times, N, K):
+        """
+        :type times: List[List[int]]
+        :type N: int
+        :type K: int
+        :rtype: int
+        """
+
+def test():
+    times = [[2,1,1],[2,3,1],[3,4,1]]
+    N = 4
+    K = 2
+    s = Solution()
+    s.networkDelayTime(times,N,K)
