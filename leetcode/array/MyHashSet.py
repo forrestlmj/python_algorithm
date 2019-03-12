9467c5cfbbd4664bf7a582afc45d2a3db5105d07
forrestlmj <18810276930@163.com>
2019-03-12 23:53:15 +0800

    hashset

--- leetcode/array/MyHashSet.py
+++ leetcode/array/MyHashSet.py
@@ -1,31 +1,31 @@
 class MyHashSet(object):
 
-    def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.s = set()
+    def __int__(self):
+        self.size = 10000
+        self.buckets = [[] for _ in range(self.size)]
+    def contains(self,key):
+        _, idx = self._index(key)
+        return idx > 0
+    def add(self,key):
+        bucket, idx = self._index(key)
+        if idx >= 0:
+            return
+        bucket.append(key)
+    def remove(self,key):
+        bucket,idx = self._index(key)
+        if idx < 0:
+            return
+        bucket.remove(key)
+    def _hash(self,key):
+        return key % self.size
+    def _index(self,key):
+        hash = self._hash(key)
+        bucket = self.buckets[hash]
+        for i,k in enumerate(bucket):
+            if key == k:
+                return bucket,i
+        return bucket,-1
 
-    def add(self, key):
-        """
-        :type key: int
-        :rtype: None
-        """
-        self.s.add(key)
-    def remove(self, key):
-        """
-        :type key: int
-        :rtype: None
-        """
-        if self.contains(key):
-            self.s.remove(key)
-    def contains(self, key):
-        """
-        Returns true if this set contains the specified element
-        :type key: int
-        :rtype: bool
-        """
-        return key in self.s
 # Your MyHashSet object will be instantiated and called as such:
 # obj = MyHashSet()
 # obj.add(key)
