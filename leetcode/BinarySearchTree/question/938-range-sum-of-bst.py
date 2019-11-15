37a40c33122081f35e5fc9f4ef2a1f9c573cabd8
forrestlmj <18810276930@163.com>
2019-11-15 14:29:04 +0800

    二叉树特性

--- leetcode/BinarySearchTree/question/938-range-sum-of-bst.py
+++ leetcode/BinarySearchTree/question/938-range-sum-of-bst.py
@@ -4,25 +4,28 @@ class TreeNode:
         self.left = None
         self.right = None
 
+
 class Solution:
+    a = 0
+
     def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
         """
-        直接提交
+        使用二叉树的特性，左面子树的值小于右边子树的值，
         :param root:
         :param L:
         :param R:
         :return:
         """
-        l = list()
-        def dfs(root: TreeNode, L: int, R: int):
-            if not root:
-                return
-            if root.left:
-                dfs(root.left,L,R)
-            if root.val >= L and root.val <= R:
-                l.append(root.val)
-            if root.right:
-                dfs(root.right,L,R)
-        dfs(root,L,R)
-        print(l)
-        return sum(l)
+
+        def dfs(cur, l, r):
+            if not cur:
+                return None
+            if cur.val >= l and cur.val <= r:
+                self.a += cur.val
+            if l < cur.val:
+                dfs(cur.left, l, r)
+            if r > cur.val:
+                dfs(cur.right, l, r)
+
+        dfs(root, L, R)
+        return self.a
