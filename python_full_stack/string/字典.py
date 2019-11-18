2dcb1e9036bac31afb7e76edf612294d2aa29359
forrestlmj <18810276930@163.com>
2019-11-18 21:23:03 +0800

    字典

---
+++ python_full_stack/string/字典.py
@@ -0,0 +1,39 @@
+"""
+day12字典
+元素不能修改、增加、删除
+"""
+info = {
+    "k1":"v1", # 键值对
+    "k2":"v2"
+}
+# 字典的key必须是hashable的，字典、列表不能作为key
+
+info2 = {
+    True:"True",
+    False:"False"
+}
+print(info2)
+# 字典使用del删除
+
+# 迭代
+for k in info:
+    print(k,info[k])
+for k,v in info.items():
+    print(k,v)
+for k in info.keys():
+    print(k)
+for v in info.values():
+    print(v)
+
+# dict的方法：
+# fromkeys静态方法,arg1可迭代变量作为类，arg2值
+v = dict.fromkeys(["k1",1,True])
+print(v)
+v = dict.fromkeys("abcd",True)
+print(v)
+
+# get
+print(v.get("notk"))
+print(v.get("notk","default"))
+# pop 指定k,删除
+print(v.pop("k1","default"))
