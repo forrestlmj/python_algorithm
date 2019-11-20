8d0d1546d7521a0fce56b621e4952451dc326aad
forrestlmj <18810276930@163.com>
2019-11-20 11:57:31 +0800

    培训课函数:可变参数*args,**kwargs global最外层变量,nonlocal上一层变量

--- python_full_stack/函数/函数.py
+++ python_full_stack/函数/函数.py
@@ -31,3 +31,62 @@ def test2(x,*args,**kwargs):
         print(k,v)
 
 test2(1,2,3,4,y=1,d=2)
+"""
+全局变量与局部变量
+"""
+name = "1hf"
+def change_name():
+    name = "yck"
+    print("change name",name)
+change_name()
+print(name)
+"""
+加global关键字引用全局变量。
+"""
+def change_g_name():
+    global name
+    name = "1"
+    print("change name",name)
+print("globa "+name)
+change_g_name()
+print("globa "+name)
+def pr():
+    print("name",name)
+l = ["yck","wsn"]
+
+def ll():
+    # 全局变量声明放在上面
+    # global l
+    l.append(1)
+    print(l)
+ll()
+
+"""
+
+global引用的是最外层的全局变量,而不是上一层的变量
+"""
+name = "刚娘"
+def weihou():
+    name = "陈卓"
+    def weiweihou():
+        global name
+        name = "冷静"
+    weiweihou()
+    print(name)
+print(name)
+weihou()
+print(name)
+"""
+如果想要使用上一层变量，则使用关键字nonlocal
+"""
+name = "刚娘"
+def weihou():
+    name = "陈卓"
+    def weiweihou():
+        nonlocal name #nonlocal找上级变量
+        name = "冷静"
+    weiweihou()
+    print(name)
+print(name)
+weihou()
+print(name)
