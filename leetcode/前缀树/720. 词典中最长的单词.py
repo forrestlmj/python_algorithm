12967ef4131590fedec55d8de653ff5421c36bc1
forrestlmj <18810276930@163.com>
2019-12-05 14:26:23 +0800

    前缀树题目

---
+++ leetcode/前缀树/720. 词典中最长的单词.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*- 
+# @Time : 2019/12/4 下午6:35 
+# @Author : yangchengkai
+# @File : 720. 词典中最长的单词.py
+class Solution(object):
+    def longestWord(self, words):
+        """
+        :type words: List[str]
+        :rtype: str
+        """
