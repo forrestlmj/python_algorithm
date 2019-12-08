a4b2a93f605deb26999615acfe4c37d31707b290
forrestlmj <18810276930@163.com>
2019-12-08 21:33:39 +0800

    使用yield编写生成器函数

--- python_full_stack/day19/03母鸡下蛋的传说.py
+++ python_full_stack/day19/03母鸡下蛋的传说.py
@@ -2,4 +2,13 @@
 # @Time : 2019/12/3 下午9:01 
 # @Author : yangchengkai
 # @File : 03母鸡下蛋的传说.py
-# wu
+#
+def product_baozi():
+    for i in range(100):
+        yield '下蛋 %s' % i
+g = product_baozi().__iter__()
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
