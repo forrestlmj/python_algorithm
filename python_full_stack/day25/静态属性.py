6caf2edba2a5065af1b463f05ff8fb369bacf093
forrestlmj <18810276930@163.com>
2020-01-25 20:16:38 +0800

    property函数属性改为了数据属性

--- python_full_stack/day25/静态属性.py
+++ python_full_stack/day25/静态属性.py
@@ -5,9 +5,9 @@ class Room:
         self.width = width
         self.length = length
         self.height = height
-    
+    @property
     def cal_area(self):
         print('%s 住的 %s 总面积是 %d' %(self.name,self.owner,self.width * self.length * self.height))
         return 
 r1 = Room("厕所",'alex',100,100,10000)
-r1.cal_area()
+r1.cal_area
