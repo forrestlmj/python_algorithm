2c8d4f0ff03bee8207d35840e5776218e7b350c4
forrestlmj <18810276930@163.com>
2020-02-01 11:29:19 +0800

    将内存中文件写入本地

--- python_full_stack/day37-45/day37/testindx/testindx/views.py
+++ python_full_stack/day37-45/day37/testindx/testindx/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import HttpResponse
 def index(req):
     print(req.GET)
     print(req.POST)
+    for item in req.FILES:
+        fileObj = req.FILES.get(item)
+        f = open(fileObj.name,'wb')
+        iter_file = fileObj.chunks()
+        for line in iter_file:
+            f.write(line)
+        f.close()
     print(req.FILES)
     return HttpResponse("sucee")
