56c67d7aad623621af9337d67a71ab01ec622b4e
forrestlmj <18810276930@163.com>
2020-03-28 09:38:08 +0800

    模板语言

--- python_full_stack/day49-day62/day49/mysite2/blog/views.py
+++ python_full_stack/day49-day62/day49/mysite2/blog/views.py
@@ -1,20 +1,28 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
+
 import time
 # Create your views here.
+
+
 def show_times(request):
     t = time.ctime()
-    return render(request,"index.html",{"time":t})
+    name = "yck"
+    return render(request, "index.html", {"time": t, "name": name})
 
-def article_year(request,year,month):
-    return HttpResponse("this is year {0},month {1}".format(year,month))
 
-def article_month(request,month):
+def article_year(request, year, month):
+    return HttpResponse("this is year {0}, month {1}".format(year, month))
+
+
+def article_month(request, month):
     return HttpResponse("this is month{0}".format(month))
 
+
 def register(request):
+    print(request.path)
     print(request.GET.get("user"))
     print(request.GET.get("age"))
     print(request.GET.get("hobby"))
     if request.method == "POST":
         return HttpResponse("success")
-    return render(request,"register.html")
+    return render(request,  "register.html")
