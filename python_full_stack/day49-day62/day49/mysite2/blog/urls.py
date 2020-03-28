f605ae6d3f0438b08af8b019b8b729488b7ca650
forrestlmj <18810276930@163.com>
2020-03-28 09:21:09 +0800

    url放入文件中

---
+++ python_full_stack/day49-day62/day49/mysite2/blog/urls.py
@@ -0,0 +1,7 @@
+from blog import views
+from django.conf.urls import url
+urlpatterns = [
+    url(r'article/(\d{4}/(\d{2}))',views.article_year),
+    url(r'article/(?P<month>\d{4})',views.article_month),
+    url(r'register',views.register)
+]
--- python_full_stack/day49-day62/day49/mysite2/mysite2/urls.py
+++ python_full_stack/day49-day62/day49/mysite2/mysite2/urls.py
@@ -13,14 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
 from django.urls import path
 from blog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('show_times/', views.show_times),
-    url(r'article/(\d{4}/(\d{2}))',views.article_year),
-    url(r'article/(?P<month>\d{4})',views.article_month),
-    url(r'register',views.register)
+    url(r'^blog/',include('blog.urls'))
 ]
