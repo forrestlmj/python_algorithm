e1540d623419e3f922c3018916307326c04156be
forrestlmj <18810276930@163.com>
2020-04-04 10:33:40 +0800

    day51 Django单表增删改查

--- python_full_stack/day49-day62/day51/Django_ORM/app01/views.py
+++ python_full_stack/day49-day62/day51/Django_ORM/app01/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from app01.models import Book
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def addbook(request):
+    b = Book(name="python2", price=99, author="yuan", pub_date="2017-12-12")
+    b.save()
+    # return render(request, )
+    return HttpResponse("添加成功")
